ac0265195986c38e0896edf519fd1e81c0d460e3
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2024-02-24 13:28:50 -0500

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as singleSpa from 'single-spa';
+import { updateState } from './store/store.reducer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+  mainState: any = {};
+
+  constructor(private store: Store) {}
+
+  updateState() {
+    this.store.dispatch(updateState({ name: "login" }));
+    singleSpa.navigateToUrl('/example-spa/main');
+  }
+
+  getMainState() {
+    if (localStorage.getItem('MAIN')) {
+      this.mainState = localStorage.getItem('MAIN');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { globalReducer } from './store/store.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyRouteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ LOGIN: globalReducer }, {
+      metaReducers: [localStorageSync({ keys: ['LOGIN'], rehydrate: true })]
+    }),
+    StoreDevtoolsModule.instrument()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_BASE_HREF, useValue: '/example-spa/login'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
